00431d5751254b129084aa8bbf6e45ca63862016
Nikhil <nikhild12377@gmail.com>
2022-01-11 23:07:46 +0530

    Template file added

---
+++ Template.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define vb vector<bool>
+#define umii unordered_map<int, int>
+#define umci unordered_map<char, int>
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define w(t)  \
+    int t;    \
+    cin >> t; \
+    while (t--)
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    w(t)
+    {
+        int n;
+        cin >> n;
+    }
+
+    return 0;
+}
